f691360b3b571668f16f548e2f149c83cf788892
gitkrishnaa <84628880+gitkrishnaa@users.noreply.github.com>
2023-03-20 10:28:28 +0530

    1 file modified

--- template_design/cv_model/script.js
+++ template_design/cv_model/script.js
@@ -11,6 +11,12 @@
   })
 
 function model1(html_display_div){
+  const large_main_div=document.getElementById("large_main_div")
+  const small_main_div=document.getElementById("small_main_div")
+  const setting=document.getElementById("setting")
+  const range1=document.getElementById("range1")
+
+ 
   //inspired from https://www.canva.com/design/DAFds5O4B9M/PYO9i5N4gA2H6X8IbhaBaw/edit
   const main_div=document.createElement("div");// main div created wher all item will depand
   main_div.style.backgroundColor="red";
@@ -29,13 +35,19 @@ function model1(html_display_div){
   const span1=document.createElement("span");
   head_name.appendChild(span1)
   span1.innerText="Krishan Thkaur"
+  span1.style.fontSize=``
 
 
-
-
+  range1.addEventListener("input",()=>{
+    large_main_div.style.width=`${range1.value/10*3}cm`
+   
+    large_main_div.style.height=`${range1.value/10*4}cm`
+    span1.style.fontSize=`${range1.value()}`
+  })
   
-  large_main_div.innerHTML=main_div.outerHTML;
+  html_display_div.innerHTML=main_div.outerHTML;
   // note-i am usng innerhtml instead of appendChild because innerhtml just replace everything thats what i want but append just adding
 }
 
 model1(large_main_div)
+model1(small_main_div)
